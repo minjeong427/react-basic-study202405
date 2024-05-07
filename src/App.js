fe91860335c91b40f3c4a4605242496516eac726
minjeong427 <bling427@naver.com>
2024-05-07 11:58:27 +0900

    Side Effect 템플릿 및 문제상황 확인

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import CourseInput from './components/CourseGoals/CourseInput';
 import CourseList from './components/CourseGoals/CourseList';
 import AddUsers from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
-
-const DUMMY_DATA = [
-  {
-    id: 'g1',
-    text: '리액트 컴포넌트 스타일 마스터하기',
-  },
-  {
-    id: 'g2',
-    text: 'UI 프로그래밍 고수되기',
-  },
-];
+import MainHeader from './components/SideEffect/MainHeader/MainHeader';
+import Login from './components/SideEffect/Login/Login';
+import Home from './components/SideEffect/Home/Home';
 
 const App = () => {
+  // 로그인 상태를 관리하는 변수
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 화면이 리렌더링 될 때 LocalStorage를 확인해서
+  // 현재 login-flag가 존재하는지 검사
+  console.log('로그인 검사 수행!');
+  const storedLoginFlag =
+    localStorage.getItem('login-flag');
+  if (storedLoginFlag === '1') {
+    setIsLoggedIn(true);
+  }
+
+  // 서버로 로그인을 요청하는 함수. (나중에는 fetch를 통한 백엔드와의 연계가 필요.)
+  const loginHandler = (email, password) => {
+    // 로그인을 했다는 증거로 상태값 변경 및 브라우저에 로그인 값을 1로 표현해서 저장.
+    // (이전 프로젝트에서는 로그인 유지를 session을 사용함. -> 이제는 사용 불가능.)
+    localStorage.setItem('login-flag', '1'); // LocalStorage: 브라우저에서 제공하는 저장소
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div>
-      <AddUsers />
-      <UserList />
-    </div>
+    <>
+      <MainHeader />
+      <main>
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+      </main>
+    </>
   );
 };
 
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import './Button.css';
+// import styled from 'styled-components';
+import styles from './Button.module.css';
 
 // const Button = styled.button`
 // font: inherit;
@@ -23,9 +23,20 @@ import './Button.css';
 // }
 // `;
 
-const Button = ({ type, onClick, children }) => {
+const Button = ({
+  type,
+  onClick,
+  children,
+  className,
+  disabled,
+}) => {
   return (
-    <button type={type} onClick={onClick} className="button">
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${styles.button} ${className}`}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
